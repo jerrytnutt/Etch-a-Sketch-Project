7a33829d929a5daa58104430ce976e736612be15
Jerry <jerrynutt416@gmail.com>
2020-12-09 19:26:27 -0500

    update
    update

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ window.onload=function(){
  const eraserButton = document.querySelector('#eraser');
 
  
-// runColor will be called anytime a box on the grid is hovered or clicked
+// setColor will be called anytime a box on the grid is hovered over
 
 var color = 'rainbow';
-function runColor(box){
+
+function setColor(box){
   if (color == 'black'){
     
     box.style.backgroundColor = "rgb(0,0,0)";
@@ -27,12 +28,29 @@ function runColor(box){
   }else if (color == 'eraser'){
 
     box.style.backgroundColor = "rgb(255,255,255)"; 
+  }else if (color == 'shade'){
+    
+   var rgb = box.style.backgroundColor
+   console.log(rgb)
+   rgb = rgb.replace(/[^\d,]/g, '').split(',');
+   newColors = rgb.map((color) => {
+     color = parseInt(color)
+     if ((color - 26) < 0){
+       return color = 0
+     }else{
+       return color = color - 26
+     }
+   })
+   
+   console.log(newColors)
+
+   box.style.backgroundColor = "rgb(" + newColors[0] + "," + newColors[1] + "," + newColors[2] +")";
   }
   
 
 }
 
-// Each button followed by event listener and function that is called
+// Each button followed by event listener and callback function 
 
 blackButton.addEventListener("click",setBlack);
 function setBlack(){
@@ -50,7 +68,7 @@ rainbowButton.addEventListener("click",setRainbow);
 
 shadeButton.addEventListener("click",shade); 
 function shade(){
-  alert('Shade feature will be added later')
+  color = 'shade'
    
     
   }
@@ -62,21 +80,21 @@ function eraserWhite(){
   
   }
 
-var eventMode = 'mouseover'
-//click
-resizeButton.addEventListener('click',resize);
+
+resizeButton.addEventListener('click',resizeGrid);
+
 // Resize function called to set up grid
-resize()
-function resize(){
+createGrid()
+function resizeGrid(){
   
   let boxes = document.querySelectorAll('.box');
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].remove()
     }
    
-    let squares = prompt("Please enter squares");
+    let squares = prompt("Please enter grid size");
     while (squares <= 0 || squares >= 101){
-      squares = prompt("Please enter squares");
+      squares = prompt("Please enter a number between 1 and 100");
 
     }
     runLoop = (squares * squares)
@@ -84,8 +102,8 @@ function resize(){
     for (var i = 0; i < runLoop; i++) {
         const content = document.createElement('div');
         content.classList.add('box');
-        content.addEventListener(eventMode, event => {
-          runColor(content)
+        content.addEventListener('mouseover', event => {
+          setColor(content)
                   
         })
         container.style.gridTemplateColumns = "repeat("+squares+",1fr)";
@@ -94,6 +112,24 @@ function resize(){
       
 
   }
+
+function createGrid(){
+  var boxes = document.querySelectorAll('.box');
+  for (var i = 0; i < 25; i++) {
+    (function (i) {
+      var currBox = boxes[i]
+      currBox.addEventListener('mouseover', event => {
+        setColor(currBox)
+                      
+        })
+    }(i));
+    
+          
+      }
+        
+  
+    }
+
 
 }
 
