0195e0e582fb3254279a0bba0383cb06cbe21afc
Jerry <jerrynutt416@gmail.com>
2020-12-11 23:36:17 -0500

    update
    update

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ function setColor(box){
 
     box.style.backgroundColor = "rgb(255,255,255)"; 
   }else if (color == 'shade'){
-    
-   var rgb = box.style.backgroundColor
-   console.log(rgb)
-   rgb = rgb.replace(/[^\d,]/g, '').split(',');
-   newColors = rgb.map((color) => {
+  // Shading feature depending on each boxes current color
+   var currentColor = box.style.backgroundColor
+   console.log(currentColor)
+   currentColor = currentColor.replace(/[^\d,]/g, '').split(',');
+   var newColors = currentColor.map((color) => {
      color = parseInt(color)
      if ((color - 26) < 0){
        return color = 0
@@ -41,50 +41,38 @@ function setColor(box){
        return color = color - 26
      }
    })
-   
-   console.log(newColors)
-
    box.style.backgroundColor = "rgb(" + newColors[0] + "," + newColors[1] + "," + newColors[2] +")";
   }
-  
-
 }
 
 // Each button followed by event listener and callback function 
-
 blackButton.addEventListener("click",setBlack);
+
 function setBlack(){
   color = 'black'
-  
 }
 
 rainbowButton.addEventListener("click",setRainbow); 
 
- function setRainbow(){
-   color = 'rainbow'
-  
+function setRainbow(){
+  color = 'rainbow'
 }
 
 
 shadeButton.addEventListener("click",shade); 
+
 function shade(){
   color = 'shade'
-   
-    
-  }
-
+}
 
 eraserButton.addEventListener('click',eraserWhite);
+
 function eraserWhite(){
   color = 'eraser'
-  
-  }
-
+}
 
 resizeButton.addEventListener('click',resizeGrid);
 
-// Resize function called to set up grid
-createGrid()
 function resizeGrid(){
   
   let boxes = document.querySelectorAll('.box');
@@ -95,42 +83,37 @@ function resizeGrid(){
     let squares = prompt("Please enter grid size");
     while (squares <= 0 || squares >= 101){
       squares = prompt("Please enter a number between 1 and 100");
-
-    }
+   }
     runLoop = (squares * squares)
     
     for (var i = 0; i < runLoop; i++) {
         const content = document.createElement('div');
         content.classList.add('box');
+        content.style.backgroundColor = "rgb(255,255,255)"; 
         content.addEventListener('mouseover', event => {
           setColor(content)
-                  
         })
         container.style.gridTemplateColumns = "repeat("+squares+",1fr)";
         container.appendChild(content)
       }
-      
-
   }
 
+// Initial grid creation when page first opens
+createGrid()
 function createGrid(){
   var boxes = document.querySelectorAll('.box');
   for (var i = 0; i < 25; i++) {
     (function (i) {
       var currBox = boxes[i]
+      currBox.style.backgroundColor = "rgb(255,255,255)"; 
       currBox.addEventListener('mouseover', event => {
         setColor(currBox)
                       
         })
     }(i));
     
-          
-      }
-        
-  
-    }
-
-
+   }
+  }
 }
 
 
